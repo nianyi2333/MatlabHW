abb6894be0f3e45705726fa9fdf08e9f9443540a
wangtianc <33483045+wangtianc@users.noreply.github.com>
2017-11-10 13:16:00 +0800

    Add files via upload
    作业1比较计算近似值与系统自带值的相对误差；
    作业2先定义一个全0矩阵然后按行按列修改

---
+++ zuoye1.m
@@ -0,0 +1,5 @@
+ pi_0=(((2*2^0.5)/9801)*1103)^(-1);
+ pi_1=(((2*2^0.5)/9801)*(1103+(4*3*2*(1103+26390))/396^4))^(-1);
+ Relative_error0=abs(pi-pi_0)/pi;
+ Relative_error1=abs(pi-pi_1)/pi;
+ pi_0,pi_1,Relative_error0,Relative_error1
