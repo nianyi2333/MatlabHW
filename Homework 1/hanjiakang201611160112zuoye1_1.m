7f81fa1ef7b163508894b68e634cddee2aee2a6a
hanjiakang <33483638+hanjiakang@users.noreply.github.com>
2017-11-11 00:37:15 +0800

    作业1_1是那个比较pi的作业2则是矩阵的
    作业一我的想法是把公式分块，重复使用的常系数先算出来存在con里，后面的变量先算n=0的存在n0，再算n=1的，再把n=1的加上n=0的作为n=1的最终值存在n1里，接着按公式计算，最后用result0，result1把结果存下来，然后计算与pi的差值，如果想看到更长的位数，可以format成long模式，我这里没有改成long模式主要因为回家以后MATLAB没法用也懒得下vpn，就这样吧。
    第二题，我分析了一下，第一第三行前五个是1-5的自然数，第四行前五个是0-8的2为步长的等差数列，最后一行是乱码，右数第一行是全1，于是我建立一个零矩阵4*5，然后分别在对其行和列进行处理，最后打出矩阵，不过老师因为我的matlab打不开，我不知道能不能用linspace处理个别行或列元素，希望老师能够理解

---
+++ Homework 1/hanjiakang201611160112zuoye1_1.m
@@ -0,0 +1,17 @@
+con=2*sqrt(2)/9801;
+n0=1103;
+quan0=con*n0;
+reslut0=1/quan0;
+n1=(4*3*2*1)*(1103+26390)/(396^4);
+n1=n1+n0;
+quan1=con*n1;
+result1=1/quan1;
+telta0=abs(reslut0);
+telta1=abs(reslut1);
+result0
+reslut1
+telta0
+telta1
+
+
+
