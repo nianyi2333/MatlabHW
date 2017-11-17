ecc7cb4d2fed9e4bd6cae3fd4d9ba5a2c344e593
qy-sunyang <455151128@qq.com>
2017-11-17 14:52:12 +0800

    homework2
    修改注释

--- Homework2/matlab_22.m
+++ Homework2/matlab_22.m
@@ -4,7 +4,7 @@ function[l_mean,b]=matlab_22(ra,dej)
 %dej=-28.936;
 GP=[192.85948,27.12825];
 lcp=122.932;
-k=180/pi;%????
+k=180/pi;%rad->degree
 ra=ra/k;
 dej=dej/k;
 GP=GP./k;
@@ -18,21 +18,21 @@ cos_lcp_l=(cos(GP(2))*sin(dej)-sin(GP(2))*cos(dej)*cos(ra-GP(1)))/cosb;
 
 b=abs(acos(cosb));
 
-lcp_l_sin=asin(sin_lcp_l)*k;%?asin???lcp-l
-lcp_l_cos=acos(cos_lcp_l)*k;%?acos???lcp-l
+lcp_l_sin=asin(sin_lcp_l)*k;%lcp-l(from sin)
+lcp_l_cos=acos(cos_lcp_l)*k;%lcp-l(from cos)
 
 
-%??(asin?acos???????)
-if sin_lcp_l>0&cos_lcp_l<0%????
+%Determine the quadrant to calculate the correct angle
+if sin_lcp_l>0&cos_lcp_l<0%the second quadrant
     lcp_l_sin=180-lcp_l_sin;
-elseif sin_lcp_l<0&cos_lcp_l<0%????
+elseif sin_lcp_l<0&cos_lcp_l<0%third
     lcp_l_sin=270-lcp_l_sin;
     lcp_l_cos=360*lcp_l_cos
-elseif sin_lcp_l<0&cos_lcp_l>0%????
+elseif sin_lcp_l<0&cos_lcp_l>0%forth
     lcp_l_cos=-lcp_l_cos;
 end
 
 lsin=lcp*k-lcp_l_sin;
 lcos=lcp*k-lcp_l_cos;
-l_mean=(lsin+lcos)/2;%????
+l_mean=(lsin+lcos)/2;%error elimination
 end
