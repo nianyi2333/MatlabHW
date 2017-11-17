f73722613e3f2fb94f373af8b84323b8556bafd1
DengDingshan <ddskevin@qq.com>
2017-11-17 14:34:16 +0800

    HOME WORK2
    this is the final draft

--- Homework 2/eqtogal.m
+++ Homework 2/eqtogal.m
@@ -15,10 +15,28 @@ si_lc = ( cos(delta) * sin(alpha - alpha_GP) )...
 co_lc = ( cos(delta_GP) * sin(delta) ...
     - sin(delta_GP) * cos(delta) * cos(alpha - alpha_GP) )...
     / ( cos(b) );
-l1 = l_CP/k - asin(si_lc)/k;
-l2 = l_CP/k - acos(co_lc)/k;
-l = (l1 + l2) / 2;
-display([b,l,l1,l2])
+
+% then we need to difine the True l is in which quadrant
+% and change the cos or sin to the write figure
+% THIS IS TO SOLVE SOMETIMES THE PROGRAM WILL CONFUSED IN THE TRUE L
+% BECAUSE IT CAN NOT KNOW THE RIGHT QUADRANT AND THE ASIN AND ACOS MAY GIVE
+% THE WRONG ANSWER
+key = [si_lc >= 0 , co_lc >= 0];
+if key(1) == 1 && key(2) == 1 %quadrant 1
+    theta1 = asin(si_lc); theta2 = acos(co_lc);
+elseif key(1) == 1 && key(2) == 0 %quadrant 2 (cos is right but sin is not)
+    theta1 = pi - asin(si_lc) ; theta2 = acos(co_lc);
+elseif key(1) == 0 && key(2) == 0 %quadrant 3 (both nor right)
+    theta1 = 1.5 * pi - asin(si_lc) ; theta2 = 2 * pi - acos(co_lc);
+elseif key(1) == 0 && key(2) == 1 %quadrant 4 (sin right but cos is not)
+    theta1 = asin(si_lc) ; theta2 = -1 * acos(co_lc);
+else 
+    display('maybe something is wrong')
 end
-% not finished 
-% something wrong with how to choose l and b
+
+l1 = l_CP/k - theta1/k;
+l2 = l_CP/k - theta2/k;
+
+l = (l1 + l2) / 2;
+end
+% finished now_ 2017 11 17 14:28
--- Homework 2/work2_2.m
+++ Homework 2/work2_2.m
@@ -1,4 +1,5 @@
 % see this work in eqtogal.m in this fold
 clc; clear; close all;
-ra = 266.405; dec = -28.936;
+ra = 266.405; dec = -28.936; %输入银心的赤经赤纬作为尝试
 [l,b] = eqtogal(ra,dec);
+display([l,b])
