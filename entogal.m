a59247ffec2a0e4bffe78ed99dfaecfaed3ede48
lycheejk <33482935+lycheejk@users.noreply.github.com>
2017-11-17 23:39:49 +0800

    homework2_刘语晨
    1.恒星光度
    2.赤道坐标和银道坐标转换

---
+++ entogal.m
@@ -0,0 +1,22 @@
+function [l,b]=entogal(ra,dec);
+raGP=192.85948;deltaGP=27.12825;
+lcp=122.932;
+%????????
+b=asind(sind(deltaGP)*sind(dec)+...
+    cosd(deltaGP)*cosd(dec)*cosd(ra-raGP))
+%????
+l1_1=asind(cosd(dec)*sind(ra-raGP)/cosd(b))
+l1_2=180-l1_1
+%sin?180-x?=sinx
+l2_1=acosd((cosd(deltaGP)*sind(dec)-...
+    sind(deltaGP)*cosd(dec)*cosd(ra-raGP))/cosd(b))
+l2_2=-l2_1
+%cos(-x?=cosx
+%????????????????????????
+if (abs(l1_1-l2_1)<0.01) || (abs(l1_1-l2_2)<0.01);
+    l=lcp-l1_1;
+elseif (abs(l1_2-l2_1)<0.01) || (abs(l1_2-l2_2)<0.01);
+    l=lcp-l1_2;
+end
+%??sin?cos??????????????????
+display([l,b])
---
+++ mass_light.m
@@ -0,0 +1,17 @@
+Mass=linspace(0.1,30,1000);
+%Mass???0.1-30????1000????
+for n=1:1000;
+    term=Mass(n);
+    if term<0.43;
+        light(n)=23*term^2.3;
+    elseif (0.43<term)&&(term<2);
+        light(n)=term^4;
+    elseif (2<term)&&(term<20);
+        light(n)=1.5*term^3.5;
+    else 20<term;
+        light(n)=3200*term;
+    end
+end
+%?????????????
+[Mass',light']
+%??????????????
