861d08cc44fdaa3e4e7193e5a1f57a5065b3ff61
typhooneye <32542969+typhooneye@users.noreply.github.com>
2017-11-18 02:02:26 +0800

    jixuan2.1

--- calculate_L.m
+++ calculate_L.m
@@ -3,7 +3,7 @@ format shortE
 Msun = 1.9891e30;
 Lsun = 3.845e33;
 m = 1;
-for n = linspace(0.1,30,1000)
+for n = linspace(0.1,30,1000)%n=M/Msun，n从1到30取1000个值
 L(m,1)= n
     if n<=0.43
         L(m,2)=0.23*(n^2.3)*Lsun;
@@ -14,6 +14,7 @@ L(m,1)= n
     else
         L(m,2)=3200*n*Lsun
     end
+    %计算结果储存到L这一个1000*2的矩阵里，第一列为质量比，第二列为光度值，单位为erg/s
 m = m+1;
 end
 L
