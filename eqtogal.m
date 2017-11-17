a32150dc1b49df986c6aa705cb72b8bdaa4771e3
lingxuankongge <33482999+lingxuankongge@users.noreply.github.com>
2017-11-17 20:28:22 +0800

    Update eqtogal.m

--- eqtogal.m
+++ eqtogal.m
@@ -3,9 +3,8 @@ alphaGP=192.85948;
 deltaGP=27.12825;
 lCP=122.932;
 b=asin(sin(deltaGP*pi/180)*sin(dec*pi/180)+cos(deltaGP*pi/180)*cos(dec*pi/180)*cos(ra*pi/180-alphaGP*pi/180))/pi*180;
-l=lCP-asin(cos(dec)*sin(ra-alphaGP)/cos(b))/pi*180;
+l=lCP-180+asin(cos(dec*pi/180)*sin(ra*pi/180-alphaGP*pi/180)/cos(b*pi/180))/pi*180;
+format long
 output=[l,b];
 end
-
-
 
