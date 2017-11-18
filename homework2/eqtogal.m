064f1639f7f7fd7de00bacc566c7586fb1b78dac
lvyalong9999 <33487185+lvyalong9999@users.noreply.github.com>
2017-11-18 16:11:47 +0800

    Add files via upload

---
+++ homework2/eqtogal.m
@@ -0,0 +1,8 @@
+function [l,b] = eqtogal(ra,dec)
+alphaGP=192.85948;
+detGP=27.12825;
+lCP=122.932;
+b=asind(sind(detGP)*sind(dec)+cosd(detGP)*cosd(alphaGP-ra));
+l=-asind(cosd(dec)*sind(ra-alphaGP))/cosd(b)-lCP;
+display([l,b])
+%%天文系的东西我数学系的实在是看不太懂……也不知道这些量是什么意思。
